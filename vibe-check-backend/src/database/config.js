eb8e4c4a443d8feb2f26667fe45331c4019bbad5
GaveenA <s3766914@student.rmit.edu.au>
2023-03-27 05:23:41 +1100

    Adding config.js for database config details with process.env variables

---
+++ vibe-check-backend/src/database/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    DIALECT: process.env.DB_DIALECT
+  };
